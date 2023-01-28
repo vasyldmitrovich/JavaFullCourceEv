6220dbea6c645a985435c3ea97c993fabc6a92fb
Potapchuc <potapchuc@gmail.com>
2023-01-28 16:54:26 +0200

    add_home-work_1

---
+++ src/com/softserve/edu01/Home_Work1.java
@@ -0,0 +1,65 @@
+package com.softserve.edu01;
+
+import java.io.PrintStream;
+import java.util.Scanner;
+
+    public class Home_Work1  {
+        public static void home_Work1 (String[] args) {
+            //    Console Application project in Java
+                Scanner console = new Scanner(System.in);
+                System.out.println("Write number 'a'");
+                int a = console.nextInt();
+                System.out.println("Write number 'b'");
+                int b = console.nextInt();
+                System.out.println("a+b="+(a+b));
+                System.out.println("a-b="+(a-b));
+                System.out.println("a*b="+(a*b));
+                System.out.println("a/b="+(a/b));
+//    Output question “How are you?“
+            /*
+
+             */
+      /*  Scanner console = new Scanner(System.in);
+        System.out.println ("Enter the radius");
+        int r = console.nextInt();
+        double perimeter = 2*Math.PI*r;  //    Math.PI - число Пі - 3.14...
+        double area = 2*Math.PI*(r*r);
+        System.out.printf("Perimeter shaped like is" + " "+"%.2f", perimeter); //  "%.2f", заокруглення до 2-го значення після коми (printf)
+        System.out.printf(" end area shaped like is" + " "+ "%.2f",area);
+*/
+
+//Define String variables name and address.
+       /* Scanner console = new Scanner(System.in);
+        System.out.println ("Hi, what is your name?" );
+        String name = console.nextLine();
+        System.out.println("Where are you live," + "  "+ name +"?");
+        String live = console.nextLine();
+        System.out.println("Wat do you do," + "  "+ name +" "+ "with" + " "+live+"?");
+*/
+//Phone calls from three different countries are с1, с2 and с3
+       /* Scanner console = new Scanner(System.in);
+
+        System.out.println ("Hi, COLL PRICE: \n USA-2$, \n United Kingdom-1.5$, \n Europe-0.8$," );
+        Double USA=2.0;
+        Double UK=1.5;
+        Double Europe=1.5;
+
+        System.out.println ("You spoke with USA-      min");
+        Double tUSA  = console.nextDouble();
+        PrintStream printf = System.out.printf("Your account with USA " + "%.2f", (USA * tUSA));
+
+        System.out.print ("\n"+"You spoke with United Kingdom-      min");
+        Double tUK  = console.nextDouble();
+        System.out.printf ("Your account with United Kingdom " + "%.2f", (UK*tUK));
+
+        System.out.println ("\n"+"You spoke with United Europe-      min");
+        Double tEurope  = console.nextDouble();
+        System.out.printf ("Your account with United Europe " + "%.2f", (Europe*tEurope));
+
+        System.out.printf("\n"+"Your general conversation is " + "%.2f", (USA*tUSA)+(UK*tUK)+(Europe*tEurope)+"$");
+
+*/
+
+        }
+
+    }
