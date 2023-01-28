fb1733121fe32e24e7e1bd5da26b8d3199737fe2
Yaroslav Tkachyk <purity.xd@gmail.com>
2023-01-29 00:27:48 +0200

    Modify implementation of Task1 using Math.random()

---
+++ src/com/softserve/edu04/practical/Task1_2.java
@@ -0,0 +1,27 @@
+package com.softserve.edu04.practical;
+
+import java.util.Arrays;
+
+public class Task1_2 {
+    public static void main(String[] args) {
+        int[] array = new int[40]; // You may place any quantity of numbers you want to read from console
+        for (int i = 0; i < array.length; i++) {
+                    array[i] = (int) (Math.random()*99)+1; // generate random number from [1 to 99]
+            }
+        System.out.println("\nTotal scanned numbers is - " + array.length);
+        System.out.println("\nArray was filled with 40 random numbers in range [1 ... 99] \n" + Arrays.toString(array));
+        System.out.println("\nTotal quantity of odd numbers among all scanned numbers is - " + oddQuantity(array));
+    }
+
+
+    public static int oddQuantity(int[] numbers) {
+        int quantity = 0;
+        for (int number : numbers) {
+            if (number % 2 == 0) {
+                quantity++;
+            }
+        }
+        return quantity;
+    }
+
+}
