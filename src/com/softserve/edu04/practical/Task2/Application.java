fcdff69c7b13c02eb8da97e29015ac333363480e
Yaroslav Tkachyk <purity.xd@gmail.com>
2023-01-29 05:30:53 +0200

    Make refactoring and fix small bug.

---
+++ src/com/softserve/edu04/practical/Task2/Application.java
@@ -0,0 +1,10 @@
+package com.softserve.edu04.practical.Task2;
+
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Please enter the number of day of the week (Integer from 1 to 7):\n");
+        DayNumber.outputDayOfTheWeek();
+        System.out.println("\nSpent 2 hours and few bottles of \uD83C\uDF7A to implement this solution \uD83D\uDE0F" );
+    }
+}
--- src/com/softserve/edu04/practical/Task2/DayNumber.java
+++ src/com/softserve/edu04/practical/Task2/DayNumber.java
@@ -1,18 +1,11 @@
-package com.softserve.edu04.practical;
+package com.softserve.edu04.practical.Task2;
 
 import java.time.DayOfWeek;
 import java.time.format.TextStyle;
 import java.util.Locale;
 import java.util.Scanner;
 
-public class Task2 {
-
-    public static void main(String[] args) {
-        System.out.println("Please enter the number of day of the week (Integer from 1 to 7):\n");
-        outputDayOfTheWeek();
-        System.out.println("\nSpent 2 hours and few bottles of \uD83C\uDF7A to implement this solution \uD83D\uDE0F" );
-    }
-
+public class DayNumber {
     public static int inputDayOfTheWeek() {
         Scanner scanner = new Scanner(System.in);
         int dayOfWeek = 0;
@@ -20,6 +13,9 @@ public class Task2 {
             try {
                 dayOfWeek = Integer.parseInt(scanner.nextLine());
             } catch (NumberFormatException exception) {
+                System.out.println("You input invalid data. Please input number from 1 to 7!");
+            }
+            if (dayOfWeek < 0 || dayOfWeek > 7) {
                 System.out.println("You input invalid data. Please input number from 1 to 7!");
             }
         }
