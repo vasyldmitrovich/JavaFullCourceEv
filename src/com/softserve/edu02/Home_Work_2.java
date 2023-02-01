367fe6d5c7a1c0bdad27e64ca6e9905a800c3af9
Potapchuc <potapchuc@gmail.com>
2023-02-01 07:04:31 +0200

    Add Employee

--- src/com/softserve/edu02/Home_Work_2.java
+++ src/com/softserve/edu02/Home_Work_2.java
@@ -1,5 +1,5 @@
 package com.softserve.edu02;
-
+/*
 public class Home_Work_2 {
     public static void main(String[] args) {
         Student st0 = new Student("Petro");
@@ -23,4 +23,42 @@ public class Home_Work_2 {
         System.out.println(st1.getName() + " - " + Student.betterStudent(st1));
         System.out.println(st2.getName() + " - " + Student.betterStudent(st2));
     }
-}
+}    */
+
+import java.util.Arrays;
+
+/*
+        EMPLOYEE
+         */
+        public class Home_Work_2 {
+            public static void main(String[] args) {
+                Employee  em0 = new Employee ("Vira");
+                Employee  em1 = new Employee ("Olga");
+                Employee  em2 = new Employee("Vita");
+
+               /* em0.setRate(12,6);
+                em1.setRating(79);
+                em2.setRating(68);*/
+
+                em0.setHours(140);
+                em1.setHours(160);
+                em2.setHours(120);
+
+                System.out.println("\nTotal employee is " + Employee.getTotalSum() + ":\n");
+                System.out.println(em0);
+                for (Employee employee : Arrays.asList(em1, em2)) {
+                    System.out.println(employee);
+                }
+
+                System.out.println("\nTota hours is " + Employee.getTotalHours());
+                System.out.println("Average hours of all employee is " + Employee.getAvrHours() + "\n");
+
+                System.out.println (" Why is the best employee?");
+                System.out.println(em0.getName() + " - "+ Employee.betterEmployee(em0));
+                System.out.println(em1.getName() + " - " + Employee.betterEmployee(em1));
+                System.out.println(em2.getName() + " - " + Employee.betterEmployee(em2));
+            }
+        }
+
+
+
