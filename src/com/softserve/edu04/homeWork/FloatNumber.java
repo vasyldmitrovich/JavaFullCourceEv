ce197f5eb8db9d23af4110b52169c576d8ca94cf
Potapchuc <potapchuc@gmail.com>
2023-02-19 11:09:12 +0200

    Add floatNumber

---
+++ src/com/softserve/edu04/homeWork/FloatNumber.java
@@ -0,0 +1,19 @@
+package com.softserve.edu04.homeWork;
+
+import java.util.Scanner;
+
+public class FloatNumber<floatNumber> {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        float max = 5;
+        float min = -5;
+        float floatNumber = sc.nextFloat();
+
+        if ((floatNumber >= -5) && (floatNumber <= 5)) {
+            System.out.println(floatNumber);
+        } else
+            System.out.println("Error enter a number between -5 to 5");
+    }
+}
+
