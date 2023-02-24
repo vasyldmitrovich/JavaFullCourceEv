974b00d258d7d3f92181403973df6c10016872b8
Marian <marianmoh21@gmail.com>
2023-02-24 13:31:16 +0200

    I have ConcurrentModificationException and I don't know how fix it :(

--- src/com/softserve/edu10/homework/task2/Main.java
+++ src/com/softserve/edu10/homework/task2/Main.java
@@ -1,5 +1,7 @@
 package com.softserve.edu10.homework.task2;
 
+import com.softserve.edu10.homework.task3.Student;
+
 import java.util.*;
 
 public class Main {
@@ -17,8 +19,8 @@ public class Main {
     }
 
     public static void fillMap(Map<String, String> map) {
-        map.put("Havalevich", "Ivan");
-        map.put("Khmilovsky", "Stas");
+        map.put("Havalevich", "Oleh");
+        map.put("Khmilovsky", "Orest");
         map.put("Soltysak", "Oleh");
         map.put("Onyshkevich", "Tanya");
         map.put("Chipko", "Marta");
@@ -38,20 +40,46 @@ public class Main {
     }
 
     public static void checkSameKey(Map<String, String> map){
-        Set<String> firstNames = new HashSet<>(map.values());
+        Collection<String> names = map.values();
+        Set<String> namesSet = new HashSet<>(names);
 
-        if(firstNames.size() == map.size()){
-            System.out.println("\nMap has no duplicate first name");
-        } else{
-            System.out.println("\nMap has duplicate first name");
+        for(String s : namesSet) {
+            names.remove(s);
+        }
+
+        Set<String> repeatedNames = new HashSet<>(names);
+
+        System.out.println("\n--Repeated names");
+
+        for(String name : repeatedNames){
+            System.out.println(name + " is " + (Collections.frequency(names, name) + 1) + " times in map");
         }
     }
 
-    public static void deleteFirstName(Map<String, String> map, String name){
+    public static void deleteFirstName(Map<String, String> map, String name) {
         for(Map.Entry<String, String> entry : map.entrySet()){
             if(entry.getValue().equals(name)){
                 map.remove(entry.getKey());
             }
         }
+
+        // The second way, have ConcurrentModificationException as well
+
+//        Iterator<String> iterator = map.values().iterator();
+//
+//        String nextName;
+//
+//        while (iterator.hasNext()){
+//            nextName = iterator.next();
+//
+//            if(nextName.equals(name)){
+//                for(String key : map.keySet()){
+//                    if(map.get(key).equals(name)){
+//                        System.out.println("\n" + key);
+//                        map.remove(key);
+//                    }
+//                }
+//            }
+//        }
     }
 }
