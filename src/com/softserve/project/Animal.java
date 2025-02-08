01e20aa631012d8a29b5f453f71d4801168908a6
YuriiGeorge13 <yurageorge13@gmail.com>
2025-02-08 17:42:04 +0200

    add Animal.java

--- src/com/softserve/project/Animal.java
+++ src/com/softserve/project/Animal.java
@@ -8,66 +8,74 @@ import java.util.Scanner;
 
 public class Animal {
     public static void main(String[] args) {
-//        Dog jack = new Dog("jack", "black", 5);
-//        jack.input();
-//        jack.print();
-//        System.out.println(jack.toString());
-//
-//        jack.makeSound();
-//
-//        Cat miki = new Cat("miki", "brown", 9);
-//        miki.makeSound();
-//        miki.input();
-//        miki.print();
-//        System.out.println(miki.toString());
+        Dog jack = new Dog("jack", "black", 5);
+        jack.input();
+        jack.print();
+        System.out.println(jack.toString());
+
+        jack.makeSound();
+
+        Cat miki = new Cat("miki", "brown", 9);
+        miki.makeSound();
+        miki.input();
+        miki.print();
+        System.out.println(miki.toString());
 //    }
         // Creating a list of animals
-        List<Animal> animals = new ArrayList<>();
+//        List<Animal> animals = new ArrayList<>();
+//
+//        // Adding animals to the list
+//        animals.add(new Dog("Jack", "Black", 5));
+//        animals.add(new Cat("Miki", "Brown", 9));
+//        animals.add(new Dog("Buddy", "White", 3));
+//        animals.add(new Cat("Lucy", "Black", 2));
+//        animals.add(new Dog("Rocky", "Brown", 4));
 
-        // Adding animals to the list
-        animals.add(new Dog("Jack", "Black", 5));
-        animals.add(new Cat("Miki", "Brown", 9));
-        animals.add(new Dog("Buddy", "White", 3));
-        animals.add(new Cat("Lucy", "Black", 2));
-        animals.add(new Dog("Rocky", "Brown", 4));
-
-        // Printing the information of the animals with black color
-        System.out.println("Animals with black color:");
-        for (Animal animal : animals) {
-            if (animal.getColor().equalsIgnoreCase("black")) {
-                System.out.println(animal.toString());
-            }
-        }
-        try {
-            Collections.sort(animals, Comparator.comparing(Animal::getName));
-            File file = new File("animals.txt");
-            FileWriter writer = new FileWriter(file);
-            for (Animal animal : animals) {
-                writer.write(animal.getName() + " (" + animal.getType() + ")\n");
-            }
-            writer.close();
-        } catch (IOException e) {
-            System.out.println("Error writing to file: " + e.getMessage());
-        }
-        // Serialize and deserialize animal list to XML
-        try {
-            FileOutputStream fos = new FileOutputStream("animals.xml");
-            ObjectOutputStream oos = new ObjectOutputStream(fos);
-            oos.writeObject(animals);
-            oos.close();
-
-            FileInputStream fis = new FileInputStream("animals.xml");
-            ObjectInputStream ois = new ObjectInputStream(fis);
-            List<Animal> deserializedAnimals = (List<Animal>) ois.readObject();
-            ois.close();
-
-            System.out.println("Deserialized animals:");
-            for (Animal animal : deserializedAnimals) {
-                System.out.println(animal.getName() + " (" + animal.getType() + ", " + animal.getColor() + ")");
-            }
-        } catch (IOException | ClassNotFoundException e) {
-            System.out.println("Error serializing/deserializing: " + e.getMessage());
-        }
+//        // Printing the information of the animals with black color
+//        System.out.println("Animals with black color:");
+//        for (Animal animal : animals) {
+//            if (animal.getColor().equalsIgnoreCase("black")) {
+//                System.out.println(animal.toString());
+//            }
+//        }
+//        Collections.sort(animals, Comparator.comparing(Animal::getName));
+//        File file = null;
+//        FileWriter writer = null;
+//
+//        try {
+//            file = new File("animals.txt");
+//            writer = new FileWriter(file);
+//            for (Animal animal : animals) {
+//                writer.write(animal.getName() + " (" + animal.getType() + ")\n");
+//            }
+//        } catch (IOException e) {
+//            System.out.println("Error writing to file: " + e.getMessage());
+//        } finally{
+//            try {
+//                writer.close();
+//            } catch (IOException e) {
+//                throw new RuntimeException(e);
+//            }
+//        }
+//        // Serialize and deserialize animal list to XML
+//        try {
+//            FileOutputStream fos = new FileOutputStream("animals.xml");
+//            ObjectOutputStream oos = new ObjectOutputStream(fos);
+//            oos.writeObject(animals);
+//            oos.close();
+//
+//            FileInputStream fis = new FileInputStream("animals.xml");
+//            ObjectInputStream ois = new ObjectInputStream(fis);
+//            List<Animal> deserializedAnimals = (List<Animal>) ois.readObject();
+//            ois.close();
+//
+//            System.out.println("Deserialized animals:");
+//            for (Animal animal : deserializedAnimals) {
+//                System.out.println(animal.getName() + " (" + animal.getType() + ", " + animal.getColor() + ")");
+//            }
+//        } catch (IOException | ClassNotFoundException e) {
+//            System.out.println("Error serializing/deserializing: " + e.getMessage());
+//        }
     }
 
 
